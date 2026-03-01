85af89a8cd0bbe8c8b747d0c1e9bdfcdc78b7a0c
fallkensmaze <amontesuruen@gmail.com>
2026-03-01 14:25:17 +0100

    .

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  publicDir: 'public'
+  publicDir: 'public',
+  base: '/' // Dominio personalizado usa base /
 })
